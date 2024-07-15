2dcd4f29d07955db152b0f5c931fb27373c9edb1
mychidarko <mickdd22@gmail.com>
2024-07-15 21:38:05 +0000

    feat: add str manipulation example

---
+++ apps/example-[typescript-expressions]/README.md
@@ -0,0 +1,12 @@
+# TypeScript Expression Example
+
+This example demonstrates how to use TypeScript basic typescript expressions and built-in type methods in your Naytive application.
+
+## How to run
+
+To run this example, simply install the dependencies and run the dev script:
+
+```bash
+pnpm install
+pnpm run dev
+```
---
+++ apps/example-[typescript-expressions]/src/index.ts
@@ -0,0 +1,17 @@
+import { std } from '@naytive/core';
+import { int } from '@naytive/types';
+
+export default function main(): int {
+  const first_name: string = 'John';
+  const first_name2: string = first_name?.toUpperCase();
+
+  std.cout(`${first_name}, type: ${typeof first_name}\n`);
+  std.cout(`${first_name2}, type: ${typeof first_name2}\n`);
+  std.cout(`${first_name.length}\n`);
+  std.cout(`${first_name.toUpperCase()}\n`);
+  std.cout(`${first_name.toLowerCase()}\n`);
+  std.cout(`${first_name[0]}\n`);
+  // std.cout(`${first_name.replace('John', 'Doe')}\n`);
+
+  return 0;
+}
